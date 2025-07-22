e7824f2a6c16df372d20b5173a880ec594814df6
RamyaKrishna1808 <ramyakrishnaa1808@gmail.com>
2025-07-22 21:07:38 +0530

    v2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:2234}
+  server: {port:2234},
+  base: '/r001/'
 })
